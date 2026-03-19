fcf5763a2fec01380bacda75253288327ca016f7
Franziska Witzel <franziska.witzel@diconium.com>
2026-03-19 13:52:50 +0100

    put back func I had discarded accidentally

--- src/runbox/auth.py
+++ src/runbox/auth.py
@@ -1,3 +1,4 @@
+import datetime as dt
 import os
 import sys
 import traceback
@@ -67,3 +68,23 @@ def connect() -> Garmin:
 
         print("\n⚠️ Login tokens invalid or Garmin backend changed behavior.")
         sys.exit("Stopping.")
+
+
+def request_data(from_date: dt.date, to_date: dt.date) -> list[dict | None]:
+    """
+    Request the running activity data from Garmin Connect.
+    """
+
+    garmin = connect()
+
+    try:
+        # input dates need to be in format '2025-07-22' - if not, the
+        # API request will fail and the error cannot be discerned from an autentication error
+        activities = garmin.get_activities_by_date(from_date.isoformat(), to_date.isoformat(), activitytype="running")
+        print(f"Fetched {len(activities)} running activities in year {from_date.year} from Garmin Connect.")
+
+        return activities
+
+    except (GarthHTTPError, GarminConnectAuthenticationError) as err:
+        print(err)
+        sys.exit("We could not connect to Garmin. The login tokens may be invalid or expired. Stopping the program.")
